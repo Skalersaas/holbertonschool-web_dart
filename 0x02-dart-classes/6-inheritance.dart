0e0eb2783054ff3bfeee95c53d24131657180a81
Skalersaas <efvhdg@gmail.com>
2024-10-25 17:19:36 +0400

    xsdf

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -5,39 +5,32 @@ class User extends Password {
   int? age;
   double? height;
   int? id;
-  String user_password = '';
 
-  User({id: int, name: String, age: int, height: double, user_password: String})
-      : super(password: user_password) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.height = height;
-    this.user_password = user_password;
-  }
+  User({required this.id, required this.name, required this.age, required this.height, required String user_password})
+      : super(password: user_password);
 
-  static User fromJson(Map<dynamic, dynamic> userJson) {
+  static User fromJson(Map<String, dynamic> userJson) {
     return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: '');
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['user_password'] ?? '',
+    );
   }
 
-  Map toJson() {
+  Map<String, dynamic> toJson() {
     return {
-      'id': this.id,
-      'name': this.name,
-      'age': this.age,
-      'height': this.height
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+      'user_password': password,
     };
   }
 
   @override
   String toString() {
-    final pass = new Password(password: this.user_password);
-
-    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})';
+    return 'User(id: $id, name: $name, age: $age, height: $height, Password valid: ${isValid()})';
   }
 }
